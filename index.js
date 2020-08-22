7128e389b5326936ae1d2177f1f8aa43c32601c4
Natasha <natasha.selvidge@gmail.com>
2020-08-22 21:37:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
-console.log(`Hello, ${specialGuest}`)
 
-var greetSpecialGuest = "" + specialGuest + "!";
+
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
 var topic = "space";
 
